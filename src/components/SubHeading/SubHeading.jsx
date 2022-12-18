81f7827a17f03854b1305770284487aedb692900
nelson <nelson3chege@gmail.com>
2022-12-18 23:31:02 +0300

    feat: implement header and aboutus section

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { images } from '../../constants';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
-  </div>
-);
-
-export default SubHeading;
+export default function SubHeading({title}) {
+  return (
+    <div className={{marginBottom:'1rem'}}>
+      <p className='p__cormorant'>{title}</p>
+      <img src={images.spoon} alt="spoon" className='spoon__img' />
+    </div>
+  )
+}
