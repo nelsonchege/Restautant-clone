d8e7f4d161973190cb3a03f5afc1354112768bc0
nelson <nelson3chege@gmail.com>
2022-12-20 01:35:31 +0300

    feat: adding video section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
 
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+export default function Intro() {
+  const [playVideo ,setPlayVideo] = useState(false)
 
-export default Intro;
+  const handleVideo =() => {
+    setPlayVideo((prevPlayVideo)=> !prevPlayVideo)
+    if(playVideo){
+      vidRef.current.pause()
+    }else{
+      vidRef.current.play()
+    }
+  }
+  const vidRef = useRef();
+  return (
+    <div className='app__video'>
+        <video ref={vidRef} src={meal} type='video/mp4' loop controls={false} muted/>
+        <div className='app__video-overlay flex__center'>
+          <div className='app__video-overlay_circle flex__center'
+               onClick={handleVideo}>
+            {playVideo ? (<BsPauseFill color='#ffff' fontSize={30}/>)
+            :(<BsFillPlayFill  color='#ffff' fontSize={30}/>)}    
+          </div>
+        </div>
+    </div>
+  )
+}
